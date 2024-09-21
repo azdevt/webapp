5a28c30f63bfc9e35dd8b3c539dc3dc5fbcc2ac3
akstf <akstf@outlook.com>
2024-09-21 22:38:34 +0530

    adding

---
+++ webapp/Modules/Functions.cs
@@ -0,0 +1,28 @@
+﻿namespace webapp.Modules
+
+
+{
+
+
+    public class Functions
+
+
+    {
+
+
+        public int Add(int x, int y)
+
+
+        {
+
+
+            return x + y;
+
+
+        }
+
+
+    }
+
+
+}
